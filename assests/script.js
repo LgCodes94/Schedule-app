6fcf993b9e013e73ad3b075ab7ccacdd870d97a9
unknown <lgcodes94@gmail.com>
2023-10-08 19:01:24 -0500

    Added index and java scirpt

--- assests/script.js
+++ assests/script.js
@@ -1,23 +1,53 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  var currentHour = dayjs().format("H");
+
+  for (var i = 9; i <= 17; i++) {
+    var time = i + " AM";
+    if (i > 12) {
+      time = (i - 12) + " PM";
+    }
+
+
+    var timeBlockHTML = `
+      <div id="hour-${i}" class="row time-block ${i < currentHour ? 'past' : (i == currentHour ? 'present' : 'future')}">
+        <div class="col-2 col-md-1 hour text-center py-3">${time}</div>
+        <textarea class="col-8 col-md-9 description" rows="3">${localStorage.getItem(`hour-${i}`) || ''}</textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+        <i class="fas fa-save" aria-hidden="true"></i>
+        </button>
+        <button class="btn deleteBtn col-2 col-md-1 bg-danger round-end" aria-label="delete">
+        <i class="fas fa-trash" aria-hidden="true"></i>
+        </button>
+        </div>
+    `;
+    $('.container-lg').append(timeBlockHTML);
+
+  }
+  $(".saveBtn").on("click", function (event) {
+    var timeBlockId = $(this).parent().attr("id");
+    var userInput = $(this).siblings(".description").val();
+    localStorage.setItem(timeBlockId, JSON.stringify(userInput));
+    $('#saveAllEntryModal').modal('show');
+  }); 
+
+  $(".deleteBtn").on("click", function () {
+    var timeBlockId = $(this).parent().attr("id");
+    localStorage.removeItem(timeBlockId);
+    $(this).siblings(".description").val('');
+  });
+
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY")); 
+});
+
+$(function () {
+  $("textarea").each(function () {
+    var task = $(this);
+    for (var i = 0; i < localStorage.length; i++) {
+      info = localStorage.key(i);
+      taskId = $(this).siblings("div").attr("id");
+      if (info == taskId) {
+        task.val(JSON.parse(localStorage.getItem(info)));
+      }
+    }
+  })
 });
